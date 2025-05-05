ac6afd0b4491b1db19c2f102443c8f24836de137
ohziiilov3 <klbaskerville0520@gmail.com>
2025-05-05 15:22:57 -0700

    add: Playlist Card container with cards

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -12,6 +12,36 @@ toggleBtn.addEventListener("click", () => {
     : '<i class="fa-solid fa-moon"></i>';
 });
 
+// create cards for platlist 
+const createPlaylistCards = (playlists) => {
+    //  console.log(data.playlists);
+  const container = document.querySelector(".playlist-cards");
+  data.playlists.forEach((playlist) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+            <img src="${playlist.playlist_art}" class="card-img-top" alt="Card Image">
+            <div class="card-overlay">
+                <i class="fas fa-play"></i>
+                <span class="play-text">Play</span>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${playlist.playlist_name}</h5>
+                <p class="card-text">${playlist.playlist_creator}</p>
+                <div class="card-review">
+                <i class="fa-regular fa-heart card-like-icon" ></i>
+                <span>${playlist.likeCount}</span>
+                </div>
+            </div>
+        `;
+    container.appendChild(card);
+
+   
+  });
+}
+
+createPlaylistCards(data.playlists); 
+
 
 const footer = document.querySelector(".footer");
 console.log(footer)
