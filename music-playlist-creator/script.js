a2a286e58fad8bac4719a548483a72fb74806972
ohziiilov3 <klbaskerville0520@gmail.com>
2025-05-05 14:48:19 -0700

    add: navbar with flex and dark mode

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,19 @@
 
+// navbar
+
+// light/dark mode
+const toggleBtn = document.getElementById("mode-toggle");
+const body = document.body;
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  toggleBtn.innerHTML = body.classList.contains("dark-mode")
+    ? '<i class="fa-solid fa-sun"></i>'
+    : '<i class="fa-solid fa-moon"></i>';
+});
+
+
+const footer = document.querySelector(".footer");
+console.log(footer)
+const currentYear = new Date().getFullYear();
+footer.innerHTML = `&copy; ${currentYear} Ampd. All rights reserved.`;
